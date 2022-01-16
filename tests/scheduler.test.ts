7119f1f443e6b76e1c594c4ee77aec9bce5fdf33
alextay96 <alextaygeeyang96@gmail.com>
2022-01-16 17:37:38 +0800

    Added working scheduler and worker

---
+++ tests/scheduler.test.ts
@@ -0,0 +1,29 @@
+import { test, expect } from "@playwright/test";
+import { Scheduler } from "../src/domain/scheduler/scheduler.js";
+import { logger } from "../src/domain/scheduler/Logger.js";
+// test("test resolve worker fp", async ({ page }) => {
+// 	const scheduler = new Scheduler();
+// 	const rawFp = Scheduler.resolveWorkerFilePath();
+
+// 	let re2 = new RegExp("./out/.*/*.js", "i");
+
+// 	expect(rawFp).toMatch(re2);
+
+// 	console.log(rawFp);
+// });
+const wait = (ms: number) => {
+	return new Promise((resolve, rej) => {
+		setTimeout(() => {
+			resolve(true);
+		}, ms);
+	});
+};
+test.only("test launch worker from scheduler", async ({ page }) => {
+	new Scheduler();
+	logger.info("start Ut");
+
+	await wait(10000);
+	logger.warn("Done Ut");
+
+	// scheduler.startWorker();
+});
