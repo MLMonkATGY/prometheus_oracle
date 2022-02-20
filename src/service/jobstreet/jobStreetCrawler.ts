940161b934cbb045f9da5e645f63a1173027dce8
alextay96 <alextaygeeyang96@gmail.com>
2022-02-20 11:15:48 +0800

    scrapeJobStreet

---
+++ src/service/jobstreet/jobStreetCrawler.ts
@@ -0,0 +1,106 @@
+import { test, expect,Page } from "@playwright/test";
+import { ServerResponse } from "http";
+import ContentFormat from "../..//domain/ContentFormat.enum.js";
+import { ConnectionManager } from "../../repo/postgres/ConnectionManager.js";
+import JobPostRaw from "../../repo/table/JobPostRaw.js";
+import fetch from 'node-fetch';
+export class JobStreetCrawler{
+
+    public scrape=async(page:Page)=>{
+        async function fetchCode(url:string) { 
+
+            let response = await fetch(url);    
+            if (response.status>= 500) {
+                console.log(" ");
+                setTimeout(async function(){
+                    await page.reload();
+        
+                },3000);
+        
+            }
+            
+        }
+        const em = await ConnectionManager(true);
+	const part1Url="https://www.jobstreet.com.my/en/job-search/computer-software-it-jobs-in-malaysia/";
+	const next_btn_s="#jobList > div > div.sx2jih0.zcydq8bm.zcydq8p > div > a";
+	
+	await page.goto("https://www.jobstreet.com.my/en/job-search/computer-software-it-jobs-in-malaysia/1/");
+		// totol pages
+		let rawNumbers = await page.locator("#pagination").innerText();
+		let pageNumElem=rawNumbers.split('\n');
+		const lastPageNum = parseInt(pageNumElem[pageNumElem.length - 1])
+		// const lastPageNum = 3
+	for (let cont_page = 2; cont_page < lastPageNum + 1; cont_page++) {
+		
+
+		const allUrls = await page.$$eval("a", (elements: any[]) =>
+		elements.map((el) => el.href)
+			);
+		const targetUrls= this.func1(allUrls);
+		const rndInt=JobStreetCrawler.randomIntFromInterval(3000,4000);
+
+			for (let cat = 0; cat < targetUrls.length; cat++) {
+				
+				await page.waitForTimeout(rndInt); 
+				const postUrl = targetUrls[cat];
+				await page.goto(postUrl).catch((err: any) => {
+					console.log(err);
+					//check status code
+					fetchCode(postUrl);
+				
+				});
+				const content_l="#contentContainer";
+				const recordNumBefore = await em.count(JobPostRaw, {});
+
+				const version = 1;
+				const portalUrl = "https://www.jobstreet.com.my";
+				const rawContents = await page.locator(content_l).innerText();
+				const rawContentType=ContentFormat.TEXT;
+				const payload = new JobPostRaw(
+					portalUrl,
+					postUrl,
+					version,
+					rawContents,
+					rawContentType
+				);
+				em.persist(payload);
+				await em.flush();
+
+				const recordNumAfter = await em.count(JobPostRaw, {});
+				expect(recordNumAfter).toBe(recordNumBefore + 1);
+				const inserted = await em.findOneOrFail(JobPostRaw, { id: payload.id });
+				expect(inserted.portalUrl).toBe(portalUrl);
+
+			}
+				const part2Url = cont_page.toString();
+				const finalUrl = part1Url.concat(part2Url);
+				await page.waitForTimeout(rndInt); 
+				await page.goto(finalUrl).catch((err: any) => {
+					console.log(err);
+					//check status code
+					fetchCode(finalUrl);
+				
+				});
+				console.log(finalUrl);
+    }
+
+
+}
+
+public func1 = (allUrls:Array<string>)=>{
+	const spec_url ="https://www.jobstreet.com.my/en/job/";
+
+	let result = allUrls.filter(function (e){
+		if(e.includes(spec_url)){
+			return true
+		}else{
+			return false
+		}
+	});
+	return result;
+}
+public static randomIntFromInterval(min: number, max: number) { // min and max included 
+	return Math.floor(Math.random() * (max - min + 1) + min)
+}}
+// const a = new JobStreetCrawler()
+// await a.scrape().catch(err=>{})
--- tests/jobstreet.test.ts
+++ tests/jobstreet.test.ts
@@ -1,7 +1,65 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, Page } from "@playwright/test";
+import { ServerResponse } from "http";
+import ContentFormat from "../src/domain/ContentFormat.enum.js";
+import { ConnectionManager } from "../src/repo/postgres/ConnectionManager.js";
+import JobPostRaw from "../src/repo/table/JobPostRaw.js";
+import fetch from 'node-fetch';
+
+
+	
+function demo(){
+					// 	// const contents = rawContents.split('\n');
+			// 	// let mp =new Map();
+
+			// 	//contents into category
+			// 	// for (let index = 0; index < contents.length; index++) {
+			// 	// 	mp.set("jobName",contents[0]);
+			// 	// 	mp.set("company",contents[1]);
+			// 	// 	mp.set("location",contents[2]);
+					
+			// 	// 	if (contents[index].includes("MYR")) {
+			// 	// 		mp.set("salary",contents[index]);
+			// 	// 	}
+			// 	// 	else if (contents[index].includes("Posted"||"Posted on")) {
+			// 	// 		mp.set("postTime",contents[index]);
+			// 	// 	}
+			// 	// 	else if (contents[index].includes("Career Level")) {
+			// 	// 		mp.set("careerLevel",contents[index+1]);
+			// 	// 	}
+			// 	// 	else if (contents[index].includes("Qualification")) {
+			// 	// 		mp.set("qualification",contents[index+1]);
+			// 	// 	}
+			// 	// 	else if (contents[index].includes("Years of Experience")) {
+			// 	// 		mp.set("yearOfExperience",contents[index+1]);
+			// 	// 	}
+			// 	// 	else if (contents[index].includes("Job Type")) {
+			// 	// 		mp.set("jobType",contents[index+1]);
+			// 	// 	}
+			// 	// 	else if (contents[index].includes("Job Specializations")) {
+			// 	// 		mp.set("jobSpecializations",contents[index+1]);
+			// 	// 	}
+			// 	// 	else if (contents[index].includes("Company Overview")) {
+			// 	// 		mp.set("companyOverview",contents[index]);
+			// 	// 	}
+			// 	// 	else if (contents[index].includes("Company Size")) {
+			// 	// 		mp.set("companySize",contents[index+1]);
+			// 	// 	}
+			// 	// 	else if (contents[index].includes("Average Processing Time")) {
+			// 	// 		mp.set("averageProcessingTime",contents[index+1]);
+			// 	// 	}
+			// 	// 	else if (contents[index].includes("Industry")) {
+			// 	// 		mp.set("industry",contents[index+1]);
+			// 	// 	}
+			// 	// 	else if (contents[index].includes("Benefits & Others")) {
+			// 	// 		mp.set("benefits",contents[index+1]);
+			// 	// 	}
+	
+			// 	// 	//TODO Company Overview, Job Description
+					
+			// 	// }
+	}
 const func1 = (allUrls:Array<string>)=>{
 	const spec_url ="https://www.jobstreet.com.my/en/job/";
-	// const result = allUrls.filter(spec_url);
 
 	let result = allUrls.filter(function (e){
 		if(e.includes(spec_url)){
@@ -12,89 +70,96 @@ const func1 = (allUrls:Array<string>)=>{
 	});
 	return result;
 }
-
-test.only("basic test", async ({ page }) => {
-
+function randomIntFromInterval(min: number, max: number) { // min and max included 
+	return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+const runTest = async (page :Page)=>{
+	const em = await ConnectionManager(true);
+	const recordNumBefore = await em.count(JobPostRaw, {});
 	const part1Url="https://www.jobstreet.com.my/en/job-search/computer-software-it-jobs-in-malaysia/";
 	const next_btn_s="#jobList > div > div.sx2jih0.zcydq8bm.zcydq8p > div > a";
 	
 	await page.goto("https://www.jobstreet.com.my/en/job-search/computer-software-it-jobs-in-malaysia/1/");
 		// totol pages
 		let rawNumbers = await page.locator("#pagination").innerText();
-		let biggest=rawNumbers.split('\n');
-
-	for (let cont_page = 1; cont_page < biggest.length+1; cont_page++) {
+		let pageNumElem=rawNumbers.split('\n');
+		const lastPageNum = parseInt(pageNumElem[pageNumElem.length - 1])
+		// const lastPageNum = 3
+	for (let cont_page = 2; cont_page < lastPageNum + 1; cont_page++) {
 		
 
 		const allUrls = await page.$$eval("a", (elements) =>
 		elements.map((el) => el.href)
 			);
 		const targetUrls = func1(allUrls);
-			
+		const rndInt=randomIntFromInterval(1000,2000);
+
 			for (let cat = 0; cat < targetUrls.length; cat++) {
 				
-				await page.waitForTimeout(1000); 
-				const exampleUrl = targetUrls[cat];
-				await page.goto(exampleUrl);
-	
+				// await page.waitForTimeout(rndInt); 
+				const postUrl = targetUrls[cat];
+				await page.goto(postUrl).catch((err) => {
+					console.log(err);
+					//check status code
+					fetchCode(postUrl);
+				
+				});
 				const content_l="#contentContainer";
+				const recordNumBefore = await em.count(JobPostRaw, {});
+
+				const version = 1;
+				const portalUrl = "https://www.jobstreet.com.my";
 				const rawContents = await page.locator(content_l).innerText();
-				const contents = rawContents.split('\n');
-				let mp =new Map();
+				const rawContentType=ContentFormat.TEXT;
+				const payload = new JobPostRaw(
+					portalUrl,
+					postUrl,
+					version,
+					rawContents,
+					rawContentType
+				);
+				em.persist(payload);
+				await em.flush();
 
-				//contents into category
-				for (let index = 0; index < contents.length; index++) {
-					mp.set("jobName",contents[0]);
-					mp.set("company",contents[1]);
-					mp.set("location",contents[2]);
-					
-					if (contents[index].includes("MYR")) {
-						mp.set("salary",contents[index]);
-					}
-					else if (contents[index].includes("Posted"||"Posted on")) {
-						mp.set("postTime",contents[index]);
-					}
-					else if (contents[index].includes("Career Level")) {
-						mp.set("careerLevel",contents[index+1]);
-					}
-					else if (contents[index].includes("Qualification")) {
-						mp.set("qualification",contents[index+1]);
-					}
-					else if (contents[index].includes("Years of Experience")) {
-						mp.set("yearOfExperience",contents[index+1]);
-					}
-					else if (contents[index].includes("Job Type")) {
-						mp.set("jobType",contents[index+1]);
-					}
-					else if (contents[index].includes("Job Specializations")) {
-						mp.set("jobSpecializations",contents[index+1]);
-					}
-					else if (contents[index].includes("Company Overview")) {
-						mp.set("companyOverview",contents[index]);
-					}
-					else if (contents[index].includes("Company Size")) {
-						mp.set("companySize",contents[index+1]);
-					}
-					else if (contents[index].includes("Average Processing Time")) {
-						mp.set("averageProcessingTime",contents[index+1]);
-					}
-					else if (contents[index].includes("Industry")) {
-						mp.set("industry",contents[index+1]);
-					}
-					else if (contents[index].includes("Benefits & Others")) {
-						mp.set("benefits",contents[index+1]);
-					}
-	
-					//TODO Company Overview, Job Description
-					
-				}
-	
+				const recordNumAfter = await em.count(JobPostRaw, {});
+				expect(recordNumAfter).toBe(recordNumBefore + 1);
+				const inserted = await em.findOneOrFail(JobPostRaw, { id: payload.id });
+				expect(inserted.portalUrl).toBe(portalUrl);
+
 			}
-		
-			const part2Url = biggest[cont_page];
-			const finalUrl = part1Url.concat(part2Url);
-			await page.goto(finalUrl);
-			console.log(finalUrl);
-
+				const part2Url = cont_page.toString();
+				const finalUrl = part1Url.concat(part2Url);
+				// await page.waitForTimeout(rndInt); 
+				await page.goto(finalUrl).catch((err) => {
+					console.log(err);
+					//check status code
+					fetchCode(finalUrl);
+				
+				});
+				console.log(finalUrl);
 	}
+
+	async function fetchCode(url:string) { 
+		let response = await fetch(url);    
+		if (response.status>= 500) {
+			console.log(" ");
+			setTimeout(async function(){
+				await page.reload();
+
+			},3000);
+
+		}
+		
+	}
+
+}
+
+test.only("basic test", async ({ page }) => {
+	await runTest(page).catch(err=>{
+		console.log(err)
+	})
+
+	
 });
+
+
